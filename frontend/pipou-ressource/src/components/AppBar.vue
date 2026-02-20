f13bba0b8ee2d8cefbf4c71019bcb9d0b17c1e06
NathanleRoux29 <nathanlerouxpro@gmail.com>
2026-02-20 10:42:01 +0100

    feat: Ajout du style en fonction de la connexion ou nom

--- frontend/pipou-ressource/src/components/AppBar.vue
+++ frontend/pipou-ressource/src/components/AppBar.vue
@@ -3,37 +3,38 @@
     <v-container fluid class="pa-0">
       <v-row no-gutters class="d-flex align-center justify-space-between justify-lg-space-around px-lg-15 py-0 my-0">
         
-        <div class="d-flex py-0 cursor-pointer" 
-          @click="$router.push('/')"
-          style="cursor: pointer;"
-        >          <v-img 
+        <div class="d-flex py-0 cursor-pointer" @click="$router.push('/')" style="cursor: pointer;">
+          <v-img 
             src="@/assets/logo.png" 
             :width="$vuetify.display.smAndDown ? 180 : 230"  
             contain
           ></v-img>
-          
           <h1 class="px-2 pt-4 d-none d-sm-flex text-md-h4 text-h5 font-weight-bold" style="line-height: 1.2;">
             Ressource <br>Relationnelle
           </h1>
         </div>
 
-        <div 
-          class="d-flex align-center cursor-pointer" 
-          @click="$router.push('/compte')"
-          style="cursor: pointer;"
-        >          
-          <h2 class="text-primary font-weight-bold mr-2 d-none d-md-flex">Stéphane</h2>
-          
-          <v-icon color="primary" :size="$vuetify.display.smAndDown ? 50 : 70">
-            mdi-account
-          </v-icon>
+        <div class="d-flex align-center">
+          <div 
+            class="d-flex align-center cursor-pointer" 
+            @click="$router.push(authLink)"
+            style="cursor: pointer;"
+          > 
+            <h2 class="text-primary font-weight-bold mr-2 d-none d-md-flex">
+              {{ authText }}
+            </h2>
+            
+            <v-icon color="primary" :size="$vuetify.display.smAndDown ? 50 : 70">
+              {{ authIcon }}
+            </v-icon>
+          </div>
 
           <v-btn
             icon="mdi-menu"
             variant="text"
             color="primary"
             class="d-md-none ml-2"
-            @click="menuModal = true"
+            @click.stop="menuModal = true"
           ></v-btn>
         </div>
       </v-row>
@@ -58,50 +59,31 @@
     </v-container>
   </v-app-bar>
 
-
-  <!--Debut du menu burger-->
-  <v-dialog v-model="menuModal" fullscreen transition="dialog-bottom-transition">
-    <v-card>
-      <v-toolbar color="white" flat>
-        <v-spacer></v-spacer>
-        <v-btn variant="text" color="primary" @click="menuModal = false" class="text-none font-weight-bold text-h6">
-          Fermer <v-icon icon="mdi-close" end></v-icon>
-        </v-btn>
-      </v-toolbar>
-
-      <v-container class="px-6">
-        <h2 class="text-primary d-flex align-center mb-2 font-weight-bold ">
-          <v-icon class="mr-2" size="small">mdi-cog</v-icon> Paramètre d'accessibilité
-        </h2>
-        
-        <v-divider class="border-opacity-100" color="primary" :thickness="2" style="max-width: 300px;"></v-divider>
-
-        <div class="d-flex flex-column">
-          <v-btn 
-            v-for="item in menuItems" 
-            :key="item.title"
-            block 
-            variant="outlined" 
-            color="grey-lighten-2"
-            class="mb-4 py-8 justify-start text-none text-black elevation-1 bg-white"
-            style="border-radius: 4px; border-color: #e0e0e0 !important;"
-            :to="item.to"
-            @click="menuModal = false"
-          >
-            <span class="text-h6 font-weight-medium text-black">{{ item.title }}</span>
-          </v-btn>
-        </div>
-      </v-container>
-    </v-card>
-  </v-dialog>
-</template>
+  </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
 const menuModal = ref(false)
+
+// GESTION DE L'AUTHENTIFICATION
+// TODO : Gestion de l'authentification avec le store
+const isLoggedIn = ref(false) 
+const userName = ref('Stéphane')
+
+const authText = computed(() => {
+  return isLoggedIn.value ? userName.value : 'Connexion'
+})
+
+const authLink = computed(() => {
+  return isLoggedIn.value ? '/compte' : '/connexion'
+})
+
+const authIcon = computed(() => {
+  return isLoggedIn.value ? 'mdi-account' : 'mdi-account-outline'
+})
 
 const menuItems = [
   { title: 'Accueil', to: '/' },
