9129e951f406508710889051da65f7834d9d7b44
NathanleRoux29 <nathanlerouxpro@gmail.com>
2026-02-20 10:41:04 +0100

    feat: Ajout de la partie AppBar

--- frontend/pipou-ressource/components.d.ts
+++ frontend/pipou-ressource/components.d.ts
@@ -11,6 +11,7 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    AppBar: typeof import('./src/components/AppBar.vue')['default']
     AppFooter: typeof import('./src/components/AppFooter.vue')['default']
     HelloWorld: typeof import('./src/components/MonComponent.vue')['default']
     MonComponent: typeof import('./src/components/MonComponent.vue')['default']
